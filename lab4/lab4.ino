23ce97c65d91751981cb8e3b95e24a25534facc9
unknown <mp65@rice.edu>
2024-02-06 15:18:12 -0600

    Printing of distances in inches & cm added

--- lab4/lab4.ino
+++ lab4/lab4.ino
@@ -3,6 +3,9 @@ const int echoPin = 12;
 long duration;
 int distanceCm, distanceInch;
 
+int R1 = 3.43e10/2;
+int R2 = 1.35039370079e10/2;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(trigPin, OUTPUT);
@@ -19,4 +22,13 @@ void loop() {
   digitalWrite(trigPin, LOW);
 
   duration = pulseIn(echoPin, HIGH);
+
+  distanceCm = duration * R1;
+  distanceInch = duration * R2;
+  Serial.print("Distance: ");
+  Serial.print(distanceCm);
+  Serial.print(" cm/");
+  Serial.print(distanceInch);
+  Serial.println(" in");
+  delay(1000);
 }
